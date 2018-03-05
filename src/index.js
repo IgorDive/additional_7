d99a511abbde2ac6ef39f71bd2a220e770bf4830
IgorDive <ihar.kazlovich@gmail.com>
2018-03-05 19:16:59 +0300

    second modified src/index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 module.exports = function solveSudoku(matrix) {
   let newMatrix = matrix.filter( arr => true ); 
-  let sumHoriz = newMatrix.map( (arr) => arr.reduce( (prevVal, value) => prevVal += value ) );
-  let sumVert = newMatrix.map( (arr, i, matr) => matr.reduce( (prevVal, arrSec) => prevVal += arrSec[i], 0 ) );
+  //let sumHoriz = newMatrix.map( (arr) => arr.reduce( (prevVal, value) => prevVal += value ) );
+  //let sumVert = newMatrix.map( (arr, i, matr) => matr.reduce( (prevVal, arrSec) => prevVal += arrSec[i], 0 ) );
   let elemFrequence = [[1, 0],[2, 0],[3, 0],[4, 0],[5, 0],[6, 0],[7, 0],[8, 0],[9, 0]];
   let arrElem = [];
-  let stackInsertedElem = [];
-  let attempts = 1;
+  let stackInsertedElem = [[0, 0, 0, 0]];
+  let attempts = 0;
+  let counterAttempts = 1;
   let tempQueue = [];
   
   start(); //start condition
@@ -50,27 +51,42 @@ module.exports = function solveSudoku(matrix) {
         if ( arrElem.map( (arrJ) => arrJ[2] ).some( (item) => { if ( j === item) return true } ) ) continue;
         for ( let i = 0; i < newMatrix[j].length; i++ ){
           if ( arrElem.map( (arrJ) => arrJ[1] ).some( (item) => { if ( i === item) return true } ) || newMatrix[j][i] !== 0 || arrElem.some( (arrJ) => { if ( arrJ[3] === Math.floor( i / 3 ) && arrJ[4] === Math.floor( j / 3 ) ) return true } ) ) continue;
-          stackInsertedElem.push( [elemArr, i, j] );
+          if ( attempts ) { 
+            if ( attempts === 1 ) counterAttempts++;
+            attempts--;
+            continue; 
+          }  
+          stackInsertedElem[stackInsertedElem.length - 1][3] = counterAttempts;
+          counterAttempts = 1;
+          stackInsertedElem.push( [elemArr[0], i, j, counterAttempts] );
           newMatrix[j][i] = elemArr[0];
           counter++;
           break out;
         }
       }
     }
+
     if ( counter < 9 - elemArr[1] ) {
-      attempts++;
-      tempQueue = stackInsertedElem.slice(-attempts);
+      tempQueue = stackInsertedElem.splice( -1 );
       tempQueue.forEach( arr => newMatrix[arr[2]][arr[1]] = 0 );
-      start();// return to the previous point of the choice    
+      counterAttempts = stackInsertedElem[stackInsertedElem.length - 1][3];
+      attempts = counterAttempts;
+      elemFrequence = [[1, 0],[2, 0],[3, 0],[4, 0],[5, 0],[6, 0],[7, 0],[8, 0],[9, 0]];
+      start();
     }
-    attempts = 1;
+               
+    attempts = 0;
+    counterAttempts = 1;
   }
   
   function makeArrElem(elemFrequenceI) {
+    arrElem = [];
     for ( let j = 0; j < newMatrix.length; j++ ) {
       for ( let i = 0; i < newMatrix[j].length; i++ ) {
         if ( elemFrequenceI === newMatrix[j][i] ) arrElem.push( [elemFrequenceI, i, j, Math.floor( i / 3), Math.floor( j / 3)] );
       }
     }
   };
+
+  return newMatrix;
 };
